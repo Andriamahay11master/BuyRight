eaba9a6d0c9c1c181e792292a3210b5e235f9a00
Mahay <hirimanana@yahoo.fr>
2025-11-08 23:40:44 +0400

    change val default loader component

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 interface LoaderProps {
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
   color?: string;
 }
 
-const Loader: React.FC<LoaderProps> = ({ size = 'medium', color = '#3498db' }) => {
+const Loader: React.FC<LoaderProps> = ({
+  size = "large",
+  color = "#17cf17",
+}) => {
   const sizeMap = {
-    small: '1rem',
-    medium: '1.5rem',
-    large: '2rem'
+    small: "1rem",
+    medium: "1.5rem",
+    large: "2rem",
   };
 
   return (
     <div className="loader">
-      <FontAwesomeIcon 
-        icon={faSpinner} 
-        spin 
-        style={{ 
+      <FontAwesomeIcon
+        icon={faSpinner}
+        spin
+        style={{
           fontSize: sizeMap[size],
-          color: color
-        }} 
+          color: color,
+        }}
       />
     </div>
   );
 };
 
-export default Loader; 
+export default Loader;
